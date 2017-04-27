4409b65f31295ff1cb33b06cd44f2b53d8585830
minhbkpro <minhbkpro@gmail.com>
2017-04-27 19:29:55 +0700

    Add browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require("gulp-rename");
+var shell = require('gulp-shell');
+var browserSync = require('browser-sync').create();
 
-// sass task
-gulp.task('sass', function() {
+// sass build task
+gulp.task('sass-build', function() {
   gulp.src('sass/clear-ui.scss')
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest('dist'))
@@ -13,7 +15,24 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('dist'));
 });
 
-// run
-gulp.task('default', ['sass'], function() {
-  gulp.watch('sass/**/*.scss', ['sass']);
+// sass watch task
+gulp.task('sass-watch', function() {
+  gulp.watch('sass/**/*.scss', ['sass-build']);
 });
+
+// jekyll task
+gulp.task('jekyll', shell.task(['cd docs;jekyll build --watch']));
+
+// browserSync task
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: {baseDir: 'docs/_site'},
+    reloadDebounce: 1000
+  });
+
+  // Reloads page when some of the already built files changed:
+  gulp.watch('docs/_site/**/*.*').on('change', browserSync.reload);
+});
+
+// run
+gulp.task('default', ['sass-build', 'sass-watch', 'jekyll', 'browserSync']);
