dc140efcf26b97bf74974084dcaefc494f2e6f90
minhbkpro <minhbkpro@gmail.com>
2019-01-16 00:20:48 +0700

    Fix rgb2hex function

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,20 +1,22 @@
 $(function() {
   // add color code in color list
   $('.docs-color-list .docs-color-box').each(function() {
-    var $this = $(this);
+    let $this = $(this);
     $this.html('<span>' + rgb2hex($this.css('background-color')) + '</span>');
   });
 });
 
 /**
- * Convert rgb to hex
- * @param  string rgb
+ * Convert rgb to hex.
+ *
+ * @param rgb
+ *
  * @return string
  */
 function rgb2hex(rgb) {
-  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-  function hex(x) {
-      return ("0" + parseInt(x).toString(16)).slice(-2);
-  }
-  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+  rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+  return (rgb && rgb.length === 4) ? "#" +
+      ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+      ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+      ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
 }
