f73127d9279345d1a7c8bf1908127c8786386837
minhbkpro <minhbkpro@gmail.com>
2019-02-03 23:48:16 +0700

    Add bootstrap docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require("gulp-rename");
-var shell = require('gulp-shell');
-var browserSync = require('browser-sync').create();
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let rename = require("gulp-rename");
+let shell = require('gulp-shell');
+let browserSync = require('browser-sync').create();
+let uglify = require('gulp-uglify');
+let concat = require('gulp-concat');
+let autoprefixer = require('gulp-autoprefixer');
 
 // sass build task
 gulp.task('sass-build', function() {
   gulp.src('sass/clear-ui.scss')
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('dist/css'))
     .pipe(gulp.dest('docs/dist/css'))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
@@ -37,7 +42,7 @@ gulp.task('minify-js', function() {
   gulp.src('js/*.js')
     .pipe(concat('clear-ui.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('dist/js'));
 
   gulp.src([
     'node_modules/popper.js/dist/umd/popper.min.js',
